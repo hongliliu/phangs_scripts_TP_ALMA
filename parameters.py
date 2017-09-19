a3bf2c6b95047086d5e39357108ea96a5ccf46ad
cnherrera <herrera@iram.fr>
2017-09-19 17:25:20 +0200

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,10 +1,14 @@
 #
-# Parameters for data reduction.
-#-------------------------------
+# Parameters for individual sources for data reduction and imaging.
+#-------------------------------------------------------------------
 
 # Path for Analysis Utils
 # You can download the ALMA Analysis Utils scripts from: ftp://ftp.cv.nrao.edu/pub/casaguides/analysis_scripts.tar
 path_au    = "/users/softs/alma/jao-mirror/AIV/science"
+
+#------------------------------
+# Parameters for data reduction:
+#------------------------------
 
 # Pipeline reduced data (True or False)
 pipeline   = True
@@ -12,10 +16,10 @@ pipeline   = True
 # Additional plots, non-interactive (plots will be saved in "plots" folder)
 doplots    = True   
 
-#Number of EB files to be used
+#Number of EB files to be used (Thought to test fast some  
 doEBs      = 0   # 0 to use all EBs
 
-# Source
+# Source name (as it is written in the data)
 source     = 'NGC_4321'
 
 # Order for the baseline fitting
@@ -24,7 +28,7 @@ bl_order   = 1
 # Rest frequency of requested line in MHz (ex: "freq_rest  = 230538" for CO(2-1))
 freq_rest  = 230538.  
 
-# Range in velocity in km/s to extract the line cube
+# Range in velocity in km/s to extract the total line cube
 vel_cube   = '1320~1830'
 
 # Range in velocity in km/s to exclude the line emission from the baseline fit. You can add more than 1 line
@@ -42,7 +46,7 @@ phase_center   = 'J2000 12h22m54.8s +15d49m19s'  # False
 # Provide velocity of the source, otherwise set to "False" and coordinates will be read from the data
 source_vel_kms = 1571 # False
 
-# width in velocity and velocity resolution in km/s
+# width in velocity and velocity resolution in km/s for final image
 vwidth_kms     = 500
 chan_dv_kms    = 2.5
 
